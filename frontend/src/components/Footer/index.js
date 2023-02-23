e822ca043c7c0fe7c283a099984532b4a01289db
LSP20xx <lsperrotti@gmail.com>
2023-02-22 21:29:05 -0300

    Third commit. I have modified the footer and added links to social media

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,10 +5,10 @@ export const Footer = () => {
     return (
         <footer className="footerContainer">
             <section className='linksContainer'>
-                <i className="fab fa-github-square"></i>
-                <i className="fab fa-linkedin"></i>
-                <i className="fab fa-behance-square"></i>
-                <i className="fab fa-twitter-square"></i>
+                <a href="https://github.com/LSP20xx" rel="noreferrer" target="_blank"><i className="fab fa-github-square"></i></a>
+                <a href="https://www.linkedin.com/in/lautaroperrotti" rel="noreferrer" target="_blank"><i className="fab fa-linkedin"></i></a>
+                <a href="https://www.behance.net/lautaroperrott" rel="noreferrer" target="_blank"><i className="fab fa-behance-square"></i></a>
+                <a href="https://twitter.com/lsperrotti" rel="noreferrer" target="_blank"><i className="fab fa-twitter-square"></i></a>
             </section>
         </footer>
     );
